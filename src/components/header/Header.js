d280e93f9aff04049b46e83ad9f3e8877d2349c0
Iryna Burianovata <iryna.burianovata@gmail.com>
2022-10-18 19:36:34 +0300

    Add navigation

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { Button, Row } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux';
-import { logout } from "../store/userSlice";
-import { auth } from "../firebase";
-import { selectUser } from "../store/userSlice";
+import { logout } from "../../store/userSlice";
+import { auth } from "../../firebase";
+import { selectUser } from "../../store/userSlice";
 
 export default function Header() {
   // const [error, setError] = useState('')
@@ -18,11 +18,8 @@ export default function Header() {
 
   return (
     <>
-      <div className="py-2 mb-4">
+      <div className="py-2 mb-4 bg-white shadow">
         <Row className="header align-items-center">
-          <div className="col-sm-4">
-            <h1 className="h2"><Link className="text-decoration-none transition-300" to="/">Tasks Dashboard</Link></h1>
-          </div>
           <div className="col-sm-8 header-text">
             {user && <p className="mb-0">Welcome, {user.email}! <Button onClick={handleLogout}>Log out</Button> <Link className="transition-300" to="/update-profile">Update profile</Link></p>}
             {!user && <p className="mb-0"><Link className="transition-300" to="/login">Log in</Link> or <Link className="transition-300" to="/signup">Sign up</Link></p>}
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaTachometerAlt, FaArchive } from "react-icons/fa";
+import clsx from 'clsx';
+import styles from './styles.module.scss';
+
+const Navbar = () => {
+  return (
+    <ul className={styles.navbar} id="accordionSidebar">
+        <Link to="/" className={clsx(styles.sidebarBrand, "my-3 d-flex align-items-center justify-content-center")}>
+          <div className="sidebar-brand-icon rotate-n-15">
+            <i className="fas fa-laugh-wink"></i>
+          </div>
+          <div className={styles.sidebarBrandText}>Tasks Dashboard</div>
+        </Link>
+
+        <hr className={clsx(styles.sidebarDivider, "mb-0")} />
+
+        <li className="nav-item active">
+          <Link className={styles.navLink} to="/">
+            <FaTachometerAlt />
+            <span>Dashboard</span>
+          </Link>
+        </li>
+
+        <hr className={styles.sidebarDivider} />
+
+        <div className={styles.sidebarHeading}>
+          Projects
+        </div>
+
+        <li className="nav-item">
+          <Link className={styles.navLink} to="/">
+            <span>Main project</span>
+          </Link>
+        </li>
+
+        <li className="nav-item">
+          <Link className={styles.navLink} to="/">
+            <span>Proj 2</span>
+          </Link>
+        </li>
+
+        <hr className={clsx(styles.sidebarDivider, "mb-0")} />
+
+        <li className="nav-item">
+            <a className={clsx(styles.navLink, styles.linkCollapsed, "nav-link collapsed")} href="#" data-toggle="collapse" data-target="#collapseTwo"
+                aria-expanded="true" aria-controls="collapseTwo">
+              <FaArchive />
+              <span>Archive</span>
+            </a>
+            <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+              <div className="bg-white py-2 collapse-inner rounded">
+                <h6 className="collapse-header">Custom Components:</h6>
+                <a className="collapse-item" href="buttons.html">Buttons</a>
+                <a className="collapse-item" href="cards.html">Cards</a>
+              </div>
+            </div>
+        </li>
+    </ul>
+  )
+}
+
+export { Navbar };
