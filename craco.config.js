6d553544504bb9f133e5e27cef087c937610c2cb
Iryna Burianovata <iryna.burianovata@gmail.com>
2022-10-19 17:52:46 +0300

    Create paths aliases

---
+++ craco.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+  webpack: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/components'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@store': path.resolve(__dirname, './src/store')
+    },
+  },
+};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
-import { store } from './store/store';
+import App from '@components/app/App';
+import { store } from '@store/store';
 import { Provider } from 'react-redux';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import "./css/styles.css";
+import "@assets/css/styles.css";
+import "@assets/css/main.scss";
 
 ReactDOM.render(
   <React.StrictMode>
